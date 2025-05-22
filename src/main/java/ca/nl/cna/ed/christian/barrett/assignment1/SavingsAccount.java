0e00ce443c43e283d10efcea4b01d65a5c05f987
C. C. Barrett <ccb9952@hotmail.com>
2025-05-22 01:04:40 -0230

    Setup Assignment 1 and classes.

---
+++ src/main/java/ca/nl/cna/ed/christian/barrett/assignment1/SavingsAccount.java
@@ -0,0 +1,4 @@
+package main.java.ca.nl.cna.ed.christian.barrett.assignment1;
+
+public class SavingsAccount extends BankAccount{
+}
---
+++ src/main/java/ca/nl/cna/ed/christian/barrett/assignment1/package-info.java
@@ -0,0 +1,5 @@
+/***
+ * Assignment 1 Package
+ */
+
+package main.java.ca.nl.cna.ed.christian.barrett.assignment1;
