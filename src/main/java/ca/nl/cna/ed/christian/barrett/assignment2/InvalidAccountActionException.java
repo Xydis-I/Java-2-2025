6d73ebd91b8614a0a52564890c7ff412e1e19653
Xydis <ccb9952@hotmail.com>
2025-05-23 10:27:48 -0230

    Assignment 2 Updated.

--- src/main/java/ca/nl/cna/ed/christian/barrett/assignment2/InvalidAccountActionException.java
+++ src/main/java/ca/nl/cna/ed/christian/barrett/assignment2/InvalidAccountActionException.java
@@ -1,20 +1,41 @@
 package main.java.ca.nl.cna.ed.christian.barrett.assignment2;
 
+/**
+ * Exception representing any invalid account actions.
+ *
+ * @author christian.barrett
+ */
 public class InvalidAccountActionException extends Exception {
     private static final String MSG = "The action attempted is not valid!";
 
+    /**
+     * Constructor for invalid account exception with no params.
+     */
     public InvalidAccountActionException() {
         super(MSG);
     }
 
+    /**
+     * Constructor for invalid account exception with message override.
+     * @param message A string representing an alternate message to be displayed.
+     */
     public InvalidAccountActionException(String message) {
         super(message);
     }
 
+    /**
+     * Constructor for invalid account exception with message and cause override.
+     * @param message A string representing an alternate message to be displayed.
+     * @param cause A Throwable object representing the cause of the exception.
+     */
     public InvalidAccountActionException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * Constructor for invalid account exception with cause override.
+     * @param cause A Throwable object representing the cause of the exception.
+     */
     public InvalidAccountActionException(Throwable cause) {
         super(MSG, cause);
     }
