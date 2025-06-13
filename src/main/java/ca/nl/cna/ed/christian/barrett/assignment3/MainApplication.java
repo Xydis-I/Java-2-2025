fbeefc2391c77009019c24a844c06f3ad0813f8a
C. C. Barrett <ccb9952@hotmail.com>
2025-06-13 01:18:50 -0230

    Assignment 3 mostly completed.

--- src/main/java/ca/nl/cna/ed/christian/barrett/assignment3/MainApplication.java
+++ src/main/java/ca/nl/cna/ed/christian/barrett/assignment3/MainApplication.java
@@ -5,35 +5,82 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Test application that demonstrates the assignments' functionality.
+ *
+ * @author christian.barrett
+ */
 public class MainApplication {
 
     public static void main(String[] args) {
         System.out.println("Fun with Student Database");
 
-        //TODO Start the program and find the input file and load
-        //TODO replace this file name with input after testing
-        File testingInputFile = new File("studentsin.txt");
+        File inputFile = new File("");
+        while (!inputFile.exists()) {
+            System.out.print("Enter File Path to Load: ");
+            String filePath = System.console().readLine();
+            inputFile = new File(filePath);
+        }
+
         List<Student> students = new ArrayList<>();
 
         try {
-            students = StudentFileManager.loadStudents(testingInputFile, System.out);
+            students = StudentFileManager.loadStudents(inputFile, System.out);
         } catch (IOException e) {
             System.out.println(e.getMessage());
             System.out.println("Error loading students from file.");
         }
 
-        //students = loadTestStudents();
+        System.out.println("""
+                add - Add a new student to the list.
+                del - Remove a student form the list.
+                mod - Modify a student""");
+        System.out.print("Select an option: ");
+        String menuOption = System.console().readLine();
 
-        //TODO Print the students
-        //TODO Build interactive part of the program - show, add, remove, modify Students in List
-        //This is a loop through the student list
-        printStudents(students);
+        switch (menuOption) {
+            case "add" -> {
+                System.out.print("Add a New Student: ");
+                String newStudent = System.console().readLine();
+                String[] details = newStudent.split(" ");
+                Student student = new Student(details[0],
+                        details[1],
+                        Integer.parseInt(details[2]),
+                        details[3],
+                        details[4],
+                        Integer.parseInt(details[5]),
+                        Float.parseFloat(details[6].substring(1)));
+                students.add(student);
+            }
+            case "del" -> {
+                System.out.print("Remove a Student (1-" + students.size() + "): ");
+                int studentIndex = Integer.parseInt(System.console().readLine()) - 1;
+                students.remove(studentIndex);
+            }
+            case "mod" -> {
+                System.out.print("Modify a Student (1-" + students.size() + "): ");
+                int studentIndex = Integer.parseInt(System.console().readLine()) - 1;
+                Student student = students.get(studentIndex);
+                System.out.println("Current: " + student.toString());
+                System.out.print("Updated: ");
+                String[] details = System.console().readLine().split(" ");
+                Student newStudent = new Student(details[0],
+                        details[1],
+                        Integer.parseInt(details[2]),
+                        details[3],
+                        details[4],
+                        Integer.parseInt(details[5]),
+                        Float.parseFloat(details[6].substring(1)));
+                students.set(studentIndex, newStudent);
+            }
+            default -> System.out.println("Invalid option.");
+        }
 
-
-        //TODO End the program and save the output file
-        File testingOutputFile = new File("studentsout.txt");
+        System.out.print("Enter File Path to Save: ");
+        String outputName = System.console().readLine();
+        File outputFile = new File(outputName);
         try {
-            StudentFileManager.saveStudents(students, testingOutputFile, System.out);
+            StudentFileManager.saveStudents(students, outputFile, System.out);
         } catch (IOException e) {
             System.out.println(e);
             System.out.println("Error saving students into file.");
@@ -41,23 +88,8 @@ public class MainApplication {
     }
 
     public static void printStudents(List<Student> students) {
-        //TODO ask josh to do this with a Lambda after we cover the next chapter ;)
         for (Student student : students) {
-            //TODO add the other attributes
             System.out.printf("%s \n", student.getFirstName());
         }
     }
-
-    //For debugging purposes
-    public static List<Student> loadTestStudents(){
-        List<Student> students = new ArrayList<>();
-        Student student = new Student();
-        student.setFirstName("John");
-        students.add(student);
-        student = new Student();
-        student.setFirstName("Jane");
-        students.add(student);
-        return students;
-    }
-
 }
--- src/main/java/ca/nl/cna/ed/christian/barrett/assignment3/StudentFileManager.java
+++ src/main/java/ca/nl/cna/ed/christian/barrett/assignment3/StudentFileManager.java
@@ -5,75 +5,71 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * A class that manages Student objects that stores (and retrieves data) in the following format:
- * File structure:
- * Josh Taylor 18298394 josh.taylor@cna.nl.ca 709-555-5555 12 $562.00
- * John Doe 18298395 john.doe@cna.nl.ca 709-555-5555 7 $0.00
- * ....
+ * A class that manages Student objects by storing and retrieving them from a file.
  *
- * All variables are seperated by a space
+ * @author christian.barrett
  */
 public class StudentFileManager {
 
+    /**
+     * Static Method used to load students from a .txt file.
+     *
+     * @param file File containing list of student information.
+     * @param logStream Stream to print any internal messages to.
+     * @return A List of Student objects retrieved from the file.
+     * @throws IOException An IOException caused by an error locating/retrieving the file.
+     */
     public static List<Student> loadStudents(File file, PrintStream logStream) throws IOException {
-        List<Student> students = new ArrayList<Student>();
+        List<Student> students = new ArrayList<>();
+        FileReader fileReader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        String line;
 
-        //Note: do not handle any exceptions, only throw them
+        while ((line = bufferedReader.readLine()) != null) {
+            String[] details = line.split(" ");
+            if (Student.isValidName(details[0])
+                    && Student.isValidName(details[1])
+                    && Student.isValidStudentNumber(details[2])
+                    && Student.isValidEmail(details[3])
+                    && Student.isValidPhoneNumber(details[4])
+                    && Student.isValidCreditsAchieved(details[5])
+                    && Student.isValidBalanceOwing(details[6])) {
 
-        //Open the file (use a FileReader and a Buffer) if there is an issue through an exception
-        try (
-                FileReader fileReader = new FileReader(file);
-                BufferedReader bufferedReader = new BufferedReader(fileReader);
-        ) {
-            String line;
-            //Loop through all the lines in the file
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] details = line.split(" ");
-                //Check the data is valid and if so, create the student and add it to the list
-                if (Student.isValidName(details[0])
-                        && Student.isValidName(details[1])
-                        && Student.isValidStudentNumber(details[2])
-                        && Student.isValidEmail(details[3])
-                        && Student.isValidPhoneNumber(details[4])
-                        && Student.isValidCreditsAchieved(details[5])
-                        && Student.isValidBalanceOwing(details[6])) {
+                Student student = new Student(details[0],
+                        details[1],
+                        Integer.parseInt(details[2]),
+                        details[3],
+                        details[4],
+                        Integer.parseInt(details[5]),
+                        Float.parseFloat(details[6].substring(1)));
+                students.add(student);
 
-                    Student student = new Student(details[0],
-                            details[1],
-                            Integer.parseInt(details[2]),
-                            details[3],
-                            details[4],
-                            Integer.parseInt(details[5]),
-                            Float.parseFloat(details[6].substring(1)));
-                    students.add(student);
-
-                    //If any data is maybe print it during test but print the summary at the end to the logStream
-                    logStream.println(details[0] + " " + details[1] + " " + details[2] + " " + details[3] + " " + details[4] + " " + details[5] + " " + details[6]);
-                } else {
-                    logStream.println("Invalid Student Details.");
-                }
+                logStream.println(details[0] + " " + details[1] + " " + details[2] + " " + details[3] + " " + details[4] + " " + details[5] + " " + details[6]);
             }
         }
-        //Close out the file
+        bufferedReader.close();
+        fileReader.close();
 
         logStream.println(students.size() + " Students loaded.");
         return students;
     }
 
+    /**
+     * Static Method used to save students to a .txt file.
+     *
+     * @param students List of Student objects to be written to the file.
+     * @param file File to write list of student information to.
+     * @param logStream Stream to print any internal messages to.
+     * @throws IOException An IOException caused by an error creating/overwriting a file.
+     */
     public static void saveStudents(List<Student> students, File file, PrintStream logStream) throws IOException {
-        //Open the file
-        //Loop through the list and print to the file
-        //Close the file
-
-        try (
-                FileWriter fileWriter = new FileWriter(file);
-                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-        ){
-            for (Student student : students) {
-                bufferedWriter.write(student.toString() + "\n");
-            }
+        FileWriter fileWriter = new FileWriter(file);
+        BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+        for (Student student : students) {
+            bufferedWriter.write(student.toString() + "\n");
         }
+        bufferedWriter.close();
+        fileWriter.close();
         logStream.println("Students Saved.");
     }
-
 }
